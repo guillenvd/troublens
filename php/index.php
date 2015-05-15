00d33bf1bee7e14754c1780f313d13e1aed48bde
guillenvd <guillenvd@gmail.com>
2015-05-15 10:54:15 -0700

    Index html

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 
 $conexion->set_charset("utf8");
 
-$query = "SELECT titulo, descripcion, direccion, foto FROM reportes order by id_reporte desc";
+$query = "SELECT a.titulo as titulo, a.descripcion as descripcion, a.direccion as direccion, a.foto as foto, b.nombre as dependencia FROM reportes a, dependencia b where b.id_dependencia=a.id_dependencia order by a.id_reporte desc";
 
 $result = $conexion->query($query);
 
@@ -16,6 +16,7 @@ if ($result->num_rows > 0) {
                 <a href=\"img/upload/". $row['foto'] ."\">
                 <h1>". $row['titulo'] ."</h1>
                 <img class=\"notimg\" src=\"http://localhost/troublens/img/upload/". $row['foto'] ."\"> 
+                </a>
                 ";
         } 
         else {
@@ -26,8 +27,8 @@ if ($result->num_rows > 0) {
             <center>
             ".$img."
             </center>
-		<p><b>Descripción:</b><br>". $row['descripcion'] ."<br><b>Dirección:</b><br> ". $row['direccion'] ."</p>
-		</a>
+		<p><b>Descripción:</b><br>". $row['descripcion'] ."<br><b>Dirección:</b><br> ". $row['direccion'] ."</p> <br><b>Dependencia:</b><br> ". $row['dependencia'] ."</p>
+        </a>
 		</div>";
 	}
 } else {
