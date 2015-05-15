9a78f08f14d244f7cc92f2beae6583b1dd680644
Luis Edmundo Espinoza Larios <luis.94leel@gmail.com>
2015-05-15 08:53:12 -0700

    database modified

--- scripts/troublens.sql
+++ scripts/troublens.sql
@@ -2,10 +2,10 @@
 -- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 06, 2015 at 06:31 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-05-2015 a las 17:48:39
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `troublens`
+-- Base de datos: `troublens`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dependencia`
+-- Estructura de tabla para la tabla `dependencia`
 --
 
 CREATE TABLE IF NOT EXISTS `dependencia` (
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `dependencia` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `dependencia`
+-- Volcado de datos para la tabla `dependencia`
 --
 
 INSERT INTO `dependencia` (`id_dependencia`, `nombre`, `direccion`, `telefono`, `correo`, `latitud`, `longitud`, `foto`) VALUES
@@ -49,32 +49,31 @@ INSERT INTO `dependencia` (`id_dependencia`, `nombre`, `direccion`, `telefono`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reportes`
+-- Estructura de tabla para la tabla `reportes`
 --
 
 CREATE TABLE IF NOT EXISTS `reportes` (
+  `dependencia` varchar(30) DEFAULT NULL,
 `id_reporte` int(11) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `correo` varchar(75) DEFAULT NULL,
   `coordenadas` varchar(75) DEFAULT NULL,
   `foto` varchar(75) DEFAULT NULL,
   `descripcion` varchar(150) DEFAULT NULL,
   `titulo` varchar(30) DEFAULT NULL,
   `direccion` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `reportes`
+-- Volcado de datos para la tabla `reportes`
 --
 
-INSERT INTO `reportes` (`id_reporte`, `id_usuario`, `correo`, `coordenadas`, `foto`, `descripcion`, `titulo`, `direccion`) VALUES
-(20, NULL, NULL, NULL, NULL, 'No hay agua en la colonia Fovissste', 'No hay agua', 'En mi colonia'),
-(21, NULL, NULL, NULL, NULL, 'Hola', 'Hola', 'Hola');
+INSERT INTO `reportes` (`dependencia`, `id_reporte`, `coordenadas`, `foto`, `descripcion`, `titulo`, `direccion`) VALUES
+(NULL, 1, NULL, '1.jpg', 'Hay un incendio en la casa de atrÃ¡s.', 'Se quemÃ³ mi casa', 'MÃ¡rtires de Rio Blanco #197'),
+(NULL, 2, NULL, '2.jpg', 'No hay agua', 'No hay agua', 'Martires rio blanco #197');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -83,58 +82,62 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `correo` varchar(75) DEFAULT NULL,
   `contrasena` varchar(75) DEFAULT NULL,
   `foto` varchar(75) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `correo`, `contrasena`, `foto`) VALUES
 (1, 'lulu', 'lourdezcampero@hotmail.com', 'lulu', '1.jpg'),
 (2, 'david', 'david@gmail.com', 'asda', '2.jpg'),
-(3, 'Ã‘oÃ±o', 'yosoyelbueno@yeah.god', 'holamundo', '3.jpg');
+(3, 'Ã‘oÃ±o', 'yosoyelbueno@yeah.god', 'holamundo', '3.jpg'),
+(4, 'Hola', 'Hola@gmail.com', 'Hola', '4.jpg'),
+(5, 'Alarez', 'lubeda@hotmail.es', 'alarez', '5.jpg'),
+(6, 'Manuel Vega', 'manuelvega15@hotmail.com', '1234', '6.jpg'),
+(7, 'a', 'a@hotmail.com', 'a', '7.jpg');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `dependencia`
+-- Indices de la tabla `dependencia`
 --
 ALTER TABLE `dependencia`
  ADD PRIMARY KEY (`id_dependencia`);
 
 --
--- Indexes for table `reportes`
+-- Indices de la tabla `reportes`
 --
 ALTER TABLE `reportes`
  ADD PRIMARY KEY (`id_reporte`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
  ADD PRIMARY KEY (`id_usuario`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `dependencia`
+-- AUTO_INCREMENT de la tabla `dependencia`
 --
 ALTER TABLE `dependencia`
 MODIFY `id_dependencia` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `reportes`
+-- AUTO_INCREMENT de la tabla `reportes`
 --
 ALTER TABLE `reportes`
-MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
